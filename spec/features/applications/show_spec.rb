9553e01ad951c72cf120b7e31964626df3974399
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-02-13 12:09:11 -0700

    Feat: US_26 Complete

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,22 +1,25 @@
 require 'rails_helper'
 
-RSpec.desccribe 'as a visitor' do
+RSpec.describe 'as a visitor' do
   describe 'when I visit the applications show page it' do
 
-    let!(shelter_1) {Shelter.create(name: 'Red Lodge Shelter', city: 'Red Lodge, MT', foster_program: false, rank: 9)}
-    let!(pet_1) {Pet.create!(name: 'Sakic', breed: 'Groenendael', age: 1, adoptable: true, shelter_id: shelter_1.id)}
-    let!(pet_2) {Pet.create!(name: 'Onyx', breed: 'Standard Poodle', age: 4, adoptable: true, shelter_id: shelter_1.id)}
-    let!(application_1) {Application.create!(name: "Gunnar Sorensen", street_address: "123 Fake Street", city: "Red Lodge", state: "MT", zipcode: "59068")}
+    let!(:shelter_1) {Shelter.create(name: 'Red Lodge Shelter', city: 'Red Lodge, MT', foster_program: false, rank: 9)}
+    let!(:pet_1) {Pet.create!(name: 'Sakic', breed: 'Groenendael', age: 1, adoptable: true, shelter_id: shelter_1.id)}
+    let!(:pet_2) {Pet.create!(name: 'Onyx', breed: 'Standard Poodle', age: 4, adoptable: true, shelter_id: shelter_1.id)}
+    let!(:application_1) {Application.create!(name: "Gunnar Sorensen", address: "123 Fake Street", city: "Red Lodge", state: "MT", zipcode: "59068", description: "I'm a great dog Dad!", status: "in progress")}
 
     before :each do
       PetApplication.create!(pet: pet_1, application: application_1)
       PetApplication.create!(pet: pet_2, application: application_1)
 
+      visit "/applications/#{application_1.id}"
+    end
+
     it 'lists the attributes of an applicant and application' do
 
      expect(page).to have_content("Applicant: Gunnar Sorensen")
      expect(page).to have_content("Address: 123 Fake Street")
-     expect(page).to have_content("City: Red Lodege")
+     expect(page).to have_content("City: Red Lodge")
      expect(page).to have_content("State: MT")
      expect(page).to have_content("Zip Code: 59068")
    end
@@ -26,6 +29,7 @@ RSpec.desccribe 'as a visitor' do
      expect(page).to have_content("Pet's Applied For")
      expect(page).to have_content("Sakic")
      expect(page).to have_content("Onyx")
+     save_and_open_page
    end
 
    it 'each pet name is a link to its show page' do
@@ -36,7 +40,7 @@ RSpec.desccribe 'as a visitor' do
        expect(current_path).to eq("/pets/#{pet_1.id}")
      end
 
-     visit application_path(application_1)
+     visit "/applications/#{application_1.id}"
 
      within("#pet_#{pet_2.id}") do
        expect(page).to have_link("Onyx")
@@ -49,5 +53,5 @@ RSpec.desccribe 'as a visitor' do
 
      expect(page).to have_content("Application Status: in progress")
    end
-  end
+ end
 end
